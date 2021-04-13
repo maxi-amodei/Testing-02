3c0d997afe3230bfc273ff2fd0b0c4e3e5b56503
Maxi Amodei <maxi_amodei@hotmail.com>
2021-04-13 16:01:13 -0300

    Very simple dog class for explanatory purposes

---
+++ dog_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+require_relative 'dog'
+
+describe Dog do
+  subject { Dog.new }
+
+  describe "#bark" do
+    it 'returns "Woof!"' do
+      
+      expect(subject.bark).to eq("Woof!")
+    end
+  end
+
+  describe "#hungry?" do
+    it "returns true if hungry level is more than 5" do
+      hungry_dog = Dog.new(hunger_level: 7)
+
+      expect(hungry_dog.hungry?).to eq(true)
+    end
+    it "returns false if hungry level is less or equal to 5" do
+      hungry_dog = Dog.new(hunger_level: 5)
+
+      expect(hungry_dog.hungry?).to eq(false)
+    end
+
+  end
+end
